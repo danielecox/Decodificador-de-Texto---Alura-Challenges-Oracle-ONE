de3fd2cb344f1106a7352b74eeb50739efb93180
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2023-01-10 22:16:05 -0300

    Adjustments in Copy/Paste

--- js/copiar.js
+++ js/copiar.js
@@ -1,8 +1,6 @@
 function copiarTexto() {
-    let textoCopiado = document.querySelector(".texto-criptografado p");
-    var btnCopy = document.querySelector("#copiar");
-    btnCopy.addEventListener("click", function () {
-        navigator.clipboard.writeText(textoCopiado.innerText);
+    copiar.addEventListener("click", function () {
+        navigator.clipboard.writeText(".texto-criptografado p");
         alert("Texto copiado");
         location.reload();
     })
